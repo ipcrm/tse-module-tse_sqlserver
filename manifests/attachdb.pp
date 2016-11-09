1add8f6d31b724dc33ff87a965d46cd3873cc4f7
ipcrm <mcadorette@gmail.com>
2016-11-09 15:16:31 -0500

    trying to move ownership to tsql

--- manifests/attachdb.pp
+++ manifests/attachdb.pp
@@ -1,13 +1,13 @@
 # This defined type is to attach a zip file containing
 # mdf & ldf files into a new database within MS SQL Server 2012.
 define tse_sqlserver::attachdb (
+  $file_source,
   $mdf_file      = 'AdventureWorks2012_Data.mdf',
   $ldf_file      = 'AdventureWorks2012_log.ldf',
   $zip_file      = 'AdventureWorks2012_Data.zip',
   $dbinstance    = 'MYINSTANCE',
   $owner         = 'CloudShop',
   $dbpass        = 'Azure$123',
-  $file_source,
 ) {
   case $::tse_sqlserver::sqlserver_version {
     '2012':  {
@@ -18,6 +18,9 @@ define tse_sqlserver::attachdb (
       $data_path  = "C:\\Program Files\\Microsoft SQL Server\\MSSQL12.${dbinstance}\\MSSQL\\DATA"
       $sqlps_path = 'C:\Program Files (x86)\Microsoft SQL Server\120\Tools\PowerShell\Modules\SQLPS'
     }
+    default: {
+      abort('Invalid SQL Server Version supplied')
+    }
   }
   staging::file { $zip_file:
     source => "${file_source}\\${zip_file}",
@@ -27,19 +30,22 @@ define tse_sqlserver::attachdb (
     creates   => "${data_path}\\${mdf_file}",
     subscribe => Staging::File[$zip_file],
   }
+
   exec { "Attach ${title}":
-    command     => "import-module \'${sqlps_path}\'; invoke-sqlcmd \"USE [master] CREATE DATABASE [${title}] ON (FILENAME = \'${data_path}\\${mdf_file}\'),(FILENAME = \'${data_path}\\${ldf_file}\') for ATTACH\" -QueryTimeout 3600  -username \'sa\' -password \'${::tse_sqlserver::sql::sa_pass}\' -ServerInstance \'${::hostname}\\${dbinstance}\'",
-    provider    => powershell,
-    path        => $sqlps_path,
-    onlyif      => "import-module \'${sqlps_path}\'; invoke-sqlcmd -Query \"select [name] from sys.databases where [name] = \'${title}\';\" -ServerInstance \"${::hostname}\\${dbinstance}\"| write-error",
+    command  => "import-module \'${sqlps_path}\'; invoke-sqlcmd \"USE [master] CREATE DATABASE [${title}] ON (FILENAME = \'${data_path}\\${mdf_file}\'),(FILENAME = \'${data_path}\\${ldf_file}\') for ATTACH\" -QueryTimeout 3600  -username \'sa\' -password \'${::tse_sqlserver::sql::sa_pass}\' -ServerInstance \'${::hostname}\\${dbinstance}\'",
+    provider => powershell,
+    path     => $sqlps_path,
+    onlyif   => "import-module \'${sqlps_path}\'; invoke-sqlcmd -Query \"select [name] from sys.databases where [name] = \'${title}\';\" -ServerInstance \"${::hostname}\\${dbinstance}\"| write-error",
   }
-  exec { "Change owner of ${title}":
-    command     => "import-module \'${sqlps_path}\'; invoke-sqlcmd \"USE [${title}] ALTER AUTHORIZATION ON DATABASE::${title} TO ${owner};\" -QueryTimeout 3600 -username \'sa\' -password \'${::tse_sqlserver::sql::sa_pass}\' -ServerInstance \'${::hostname}\\${dbinstance}\'",
-    provider    => powershell,
-    onlyif      => "import-module \'${sqlps_path}\'; invoke-sqlcmd -Query \"select suser_sname(owner_sid) from sys.databases where [name] = \'${title}\';\" -ServerInstance \"$::hostname\\${dbinstance}\" | where-object \"Column1\" -eq \"${owner}\" | write-error",
-    subscribe   => Exec["Attach ${title}"],
+
+  sqlserver_tsql{ "Change Owner of ${title}":
+    instance  => "${::hostname}\\${dbinstance}",
+    command   => "USE [${title}] ALTER AUTHORIZATION ON DATABASE::${title} TO ${owner}",
+    onlyif    => "select suser_sname(owner_sid) from sys.databases where [name] = ${title}",
+    subscribe => Exec["Attach ${title}"],
   }
-  sqlserver::login{ "${owner}":
+
+  sqlserver::login{ $owner:
     instance => $dbinstance,
     password => $dbpass,
     notify   => Exec["Attach ${title}"],
